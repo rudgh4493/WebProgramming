361fbb2efadc17ae79fc134db34c7dfb0884a2a6
rudgh4493 <70314961+rudgh4493@users.noreply.github.com>
2023-11-11 18:09:58 +0900

    Update p2.php

--- 1106/assignment/php_p/p2.php
+++ 1106/assignment/php_p/p2.php
@@ -1,8 +1,27 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
     function factorial($n){       
         if($n==0) return 1;
         return $n * factorial($n-1);        
     }
 
+    echo "4! = ";
     echo factorial(4);
+    echo "\n5! = ";
+    echo factorial(5);
+    echo "\n6! = ";
+    echo factorial(6);
 ?>
+
+
+</body>
+</html>
