2d0f93fbf2ccd6a511063b99ea4cfc2cbbefb468
rudgh4493 <70314961+rudgh4493@users.noreply.github.com>
2023-11-11 18:35:58 +0900

    Update p1.php

--- 1106/assignment/php_p/p1.php
+++ 1106/assignment/php_p/p1.php
@@ -1,12 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
     function oddeven($a){        
         if($a%2==0){
@@ -27,6 +19,3 @@
     echo "13입력: ";
     oddeven(13);
 ?>
-
-</body>
-</html>
