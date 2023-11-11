87614b1781f02871273dd6d5d0853be60f404d75
rudgh4493 <70314961+rudgh4493@users.noreply.github.com>
2023-11-11 17:34:24 +0900

    Update p1.php

--- 1106/assignment/php_p/p1.php
+++ 1106/assignment/php_p/p1.php
@@ -1,3 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
     function oddeven($a){        
         if($a%2==0){
@@ -9,9 +18,15 @@
             echo "\n";
         }  
     }
-
+    echo "10입력: ";
     oddeven(10);
+    echo "11입력: ";
     oddeven(11);
+    echo "12입력: ";
     oddeven(12);
+    echo "13입력: ";
     oddeven(13);
 ?>
+
+</body>
+</html>
